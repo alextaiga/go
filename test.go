a1537edf535ecb39049c4e32ee4dbb981eb89bb4
alextaiga <toca2208@gmail.com>
2021-04-22 01:23:40 +1000

    123

--- test.go
+++ test.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/alextaiga/go/new_folder"
 )
 
 func CoolFunc() {
-	fmt.Println(new_folder.CoolFunc2() + " World!")
+	fmt.Println("Hello" + " World!")
 }
