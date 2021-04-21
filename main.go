cf9e56823bacb422bf0f4037d5c1ded32a4bd821
alextaiga <toca2208@gmail.com>
2021-04-22 01:27:04 +1000

    fixes

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func NewFunc() {
+	fmt.Println("Hello")
+}
--- test.go
+++ test.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/alextaiga/go/new_folder"
 )
 
-func CoolFunc() {
-	fmt.Println("Hello" + " World!")
+func main() {
+	fmt.Println(new_folder.CoolFunc2() + " World!")
 }
